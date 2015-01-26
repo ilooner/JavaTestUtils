4035637b27080c158843b85c695ff677bd833379
Timothy Farkas <tfarkas@david-vb.(none)>
2015-01-25 21:21:11 -0800

    Added properties for main and test resource directories.

--- src/main/java/com/topekalabs/testutils/TestUtils.java
+++ src/main/java/com/topekalabs/testutils/TestUtils.java
@@ -12,13 +12,28 @@ package com.topekalabs.testutils;
 public class TestUtils
 {
     public static final String PROPERTY_BUILD_DIRECTORY = "java.testutils.build.dir";
+    public static final String PROPERTY_RESOURCE_MAIN_DIRECTORY = "java.testutils.resources.main";
+    public static final String PROPERTY_RESOURCE_TEST_DIRECTORY = "java.testutils.resources.test";
     
     public static final String BUILD_DIR;
+    public static final String RESOURCES_MAIN_DIR;
+    public static final String RESOURCES_TEST_DIR;
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // TODO - Add annotations and set variables with a loop instead of manually
+    // setting them.
+    ////////////////////////////////////////////////////////////////////////////
     
     static
     {
         BUILD_DIR = System.getProperty(PROPERTY_BUILD_DIRECTORY);
         propertyNotSetException(PROPERTY_BUILD_DIRECTORY, BUILD_DIR);
+        
+        RESOURCES_MAIN_DIR = System.getProperty(PROPERTY_RESOURCE_MAIN_DIRECTORY);
+        propertyNotSetException(PROPERTY_RESOURCE_MAIN_DIRECTORY, RESOURCES_MAIN_DIR);
+        
+        RESOURCES_TEST_DIR = System.getProperty(PROPERTY_RESOURCE_TEST_DIRECTORY);
+        propertyNotSetException(PROPERTY_RESOURCE_TEST_DIRECTORY, RESOURCES_TEST_DIR);
     }
     
     private TestUtils()
